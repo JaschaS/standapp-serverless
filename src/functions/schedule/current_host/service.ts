04dcb588f0b97ef1242e85aa061bf3421720d1c8
Jascha Scholz <jascha_scholz@web.de>
2021-06-29 22:10:34 +0200

    start and end date will now be generated if missing

--- src/functions/schedule/current_host/service.ts
+++ src/functions/schedule/current_host/service.ts
@@ -3,6 +3,10 @@ import { Host } from "src/models/host";
 import { CurrentHost } from "src/models/current_host";
 import { createLogger } from "@libs/logger";
 
+
+// TODO: if start date is null generate end date as well
+// TODO: if end date is not given generate it from start date + 1
+
 export class CurrentHostService {
 
     constructor(private logger = createLogger('currenthost-service')) {}
@@ -11,12 +15,21 @@ export class CurrentHostService {
 
         this.logger.info(`look up for host for user ${user} from ${start} until ${end}`);
 
-        const startDate = this.convertStart(start);
-        const endDate = this.convertEnd(end);
+        let startDate = start;
+        let endDate = end;
+        if(startDate == null) {
+            startDate = this.newStart();
+            endDate = this.updateEnd(startDate);
+        }
+        else {
+            if(endDate == null) {
+                endDate = this.updateEnd(startDate);
+            }
+        }
         const hostList: Host[] = await getHostByDate(user, startDate, endDate) as Host[];
 
         if(hostList.length == 0) {
-            this.logger.info(`no host find for user ${user} from ${start} until ${end}`);
+            this.logger.info(`no host find for user ${user} from ${startDate} until ${endDate}`);
             return this.emptyHost();
         }
 
@@ -30,19 +43,22 @@ export class CurrentHostService {
         };
     }
 
-    private convertStart(start?: string) {
-        if(start == null) return start;
-
+    private newStart() {
         const newStart = new Date();
-        return newStart.toISOString();
+        return this.splitDate(newStart);
     }
 
-    private convertEnd(end?: string) {
-        if(end == null) return end;
+    private updateEnd(start: string) {
+        // use T02:00:00 because of utc time - otherwise the time may be the day before
+        // e.g. 2021-06-01T00:00:00 would be 2021-05-31T22:00:00
 
-        const today = new Date(Date.now());
+        const today = new Date(Date.parse(`${start}T02:00:00`));
         const newEnd = new Date(today.getTime() + (1 * 24 * 60 * 60 * 1000));
-        return newEnd.toISOString();
+        return this.splitDate(newEnd);
+    }
+
+    private splitDate(date: Date): string {
+        return date.toISOString().split("T")[0];
     }
 
     private emptyHost(): CurrentHost {
